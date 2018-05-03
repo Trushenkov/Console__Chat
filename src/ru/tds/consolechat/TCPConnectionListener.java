d7686f94cb613830175875f3f40cdc7b41203cf3
Trushenkov Dmitry <diman28051999@gmail.com>
2018-05-03 14:10:00 +0300

    Added comments

--- src/ru/tds/consolechat/TCPConnectionListener.java
+++ src/ru/tds/consolechat/TCPConnectionListener.java
@@ -3,16 +3,40 @@ package ru.tds.consolechat;
 import java.io.IOException;
 
 /**
+ * Интерфейс, содержащий методы для работы с соединением
+ *
  * @author Трушенков Дмитрий 15ИТ18
  */
 public interface TCPConnectionListener {
 
+    /**
+     * Метод для информирования о подключении нового соединения
+     *
+     * @param tcpConnection новое соединение
+     */
     void onConnectionReady(TCPConnection tcpConnection);
 
+    /**
+     * Метод для отправки сообщения соединением
+     *
+     * @param tcpConnection соединение, которое отправляет сообщение
+     * @param string        сообщение, которое нужно отправить
+     */
     void onReceiveString(TCPConnection tcpConnection, String string);
 
+    /**
+     * Метод для информирования об отключении соединения
+     *
+     * @param tcpConnection соединение
+     */
     void onDisconnect(TCPConnection tcpConnection);
 
+    /**
+     * Метод для обрабатывания исключения, возникшего у соединения
+     *
+     * @param tcpConnection соединение
+     * @param exception     исключение
+     */
     void onException(TCPConnection tcpConnection, IOException exception);
 
 }
